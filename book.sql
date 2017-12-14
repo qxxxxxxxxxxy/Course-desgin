d4c247413f5f1758884d0c3cc42097fa7c777578
qxxxxxxxxxxy <562952405@qq.com>
2017-12-14 10:58:33 +0800

    更新了sql文件

---
+++ book.sql
@@ -0,0 +1,116 @@
+CREATE TABLE `administrator` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`password` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`id`) 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=1;
+
+CREATE TABLE `cart` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`user_id` int(11) NULL DEFAULT NULL,
+`name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`price` double NULL DEFAULT NULL,
+`img` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`count` int(11) NULL DEFAULT NULL,
+`date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`id`) ,
+INDEX `ii` (`user_id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=5;
+
+CREATE TABLE `orders` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`count` int(11) NULL DEFAULT NULL,
+`price` double NULL DEFAULT NULL,
+`img` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`user_id` int(11) NULL DEFAULT NULL,
+PRIMARY KEY (`id`) 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=1;
+
+CREATE TABLE `product` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`price` double NULL DEFAULT NULL,
+`discount` double NULL DEFAULT NULL,
+`description` varchar(400) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`img1` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`img2` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`img3` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`img4` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`tags` varchar(400) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`productType_id` int(11) NULL DEFAULT NULL,
+PRIMARY KEY (`id`) ,
+INDEX `asdf` (`productType_id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=39;
+
+CREATE TABLE `producttype` (
+`id` int(11) NOT NULL DEFAULT 0,
+`type` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`id`) ,
+INDEX `type` (`type`),
+INDEX `id` (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `user` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`country` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`companyname` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`address` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`town` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`county` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`postcode` int(11) NULL DEFAULT NULL,
+`Email` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`password` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`id`) 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=6;
+
+CREATE TABLE `usercart` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`user_id` int(11) NULL DEFAULT NULL,
+PRIMARY KEY (`id`) 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=2;
+
+CREATE TABLE `userloginlogging` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`user_id` int(11) NULL DEFAULT NULL,
+`user_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`id`) ,
+INDEX `pro` (`user_id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=35;
+
+CREATE TABLE `userorder` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`user_id` int(11) NULL DEFAULT NULL,
+PRIMARY KEY (`id`) 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=2;
+
